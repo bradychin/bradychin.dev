fef709ab1a2b1c5e7f31b5866579c8f5892a3f62
Brady <brady@bradychin.dev>
2025-09-20 15:51:24 -0500

    Added ham menu

--- js/main.js
+++ js/main.js
@@ -60,3 +60,12 @@ window.addEventListener("scroll", () => {
     if (navClone) navClone.remove();
   }
 });
+
+// ------- Ham menu ------- //
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+hamMenu.addEventListener('click', () =>{
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
